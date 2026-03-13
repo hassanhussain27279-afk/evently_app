b91b79ac6a05752b496ca78441c0e170cde66f93
Hassan Hussain <hassanhussain.2279@gmail.com>
2026-03-14 00:23:45 +0200

    create project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EventlyApp());
+}
+
+class EventlyApp extends StatelessWidget {
+  const EventlyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
